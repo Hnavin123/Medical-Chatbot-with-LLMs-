2a730c042da8ef1425d350de568a642339ce9c68
Hnavin123 <hnavin2132002@gmail.com>
2025-11-04 12:55:38 +0530

    FOlder structure added

---
+++ template.sh
@@ -0,0 +1,11 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/__helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
